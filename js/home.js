8bd36836eb31109c1fb6835994a31742c6aedb39
Andy Baek <andybaek95@gmail.com>
2015-12-23 23:54:06 -0500

    chosen cards binding

--- js/home.js
+++ js/home.js
@@ -1,5 +1,15 @@
 angular.module('Jujitsu')
 .controller('HomeController', ['$http', '$scope', '$location', '$routeParams',
-function($http, $scope, $location, $routeParams){
+	function($http, $scope, $location, $routeParams){
 
-}]);
+		$scope.cards = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+		$scope.players = [
+		{ id: 1, name: 'John', chosenCard: "A"},
+		{ id: 2, name: 'George', chosenCard: "A"}
+		];
+
+		$scope.cardChosen = function(value, player){
+			player.chosenCard = value;
+		}
+
+	}]);
